0b2a3c15ff06bf0b76bcf31511f4dffd9daedbb5
zzeng31 <zinuozeng@gmail.com>
2023-08-01 10:25:07 -0700

    fix cors

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
+const cors = require("cors");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 8000;
 const schema = require("./schema/schema");
 const app = express();
 connectDB();
+app.use(cors());
 app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
 app.listen(port, console.log(`Server running on port ${port}`));
